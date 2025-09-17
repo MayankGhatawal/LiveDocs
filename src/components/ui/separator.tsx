57aacc9e3f156121ad59d3929051a5c54ce3d9ad
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-09-17 13:42:58 +0530

    main

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
 import { cn } from "@/lib/utils"
 
 function Separator({
@@ -17,8 +16,11 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
+        "bg-border shrink-0",
+        orientation === "horizontal"
+          ? "h-px w-full"
+          : "w-px h-auto", // allow custom height for vertical
+        className // custom overrides come last
       )}
       {...props}
     />
