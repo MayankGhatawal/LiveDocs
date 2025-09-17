067aba06e850a7746bdb20cb11d0403cace66224
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-09-17 12:54:16 +0530

    main

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { LucideIcon, Undo2Icon } from "lucide-react";
+import { channel } from "diagnostics_channel";
+import { BoldIcon, ItalicIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, SpellCheck, SpellCheck2Icon, UnderlineIcon, Undo2Icon } from "lucide-react";
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -43,11 +45,82 @@ export const Toolbar = () => {
         icon: Undo2Icon,
         onClick: () => editor?.chain().focus().undo().run(),
       },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print()
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheck,
+        onClick: () => {
+          if (!editor) return;
+          const dom = editor.view.dom as HTMLElement;
+          const current = dom.getAttribute("spellcheck");
+          const newValue = current === "true" ? "false" : "true";
+          dom.setAttribute("spellcheck", newValue);
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        isActive: editor?.isActive("bold"),
+        onClick: () => editor?.chain().focus().toggleBold().run()
+      },
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        isActive: editor?.isActive("italic"),
+        onClick: () => editor?.chain().focus().toggleItalic().run()
+      },
+      {
+        label: "Underline",
+        icon: UnderlineIcon,
+        isActive: editor?.isActive("underline"),
+        onClick: () => editor?.chain().focus().toggleUnderline().run()
+      }
+    ],
+    [
+      {
+        label: "Comment",
+        icon: MessageSquarePlusIcon,
+        isActive: false,
+        onClick: () => console.log("TODO: Comment"),
+        
+      }
     ],
   ];
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto ">
       {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* TODO: FONT FAMILY */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* TODO: HEADING */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* TODO: FONT SIZE */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      {/* TODO: Text Color */}
+      {/* TODO: Highlight Color */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* TODO: Link */}
+      {/* TODO: Image */}
+      {/* TODO: Align */}
+      {/* TODO: Line height */}
+      {/* TODO: List */}
+      {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
