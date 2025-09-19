5111e149f6797770d6e68f013e864ab685b81bce
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-09-19 14:19:20 +0530

    main

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -3,12 +3,11 @@ import Image from "next/image";
 import SearchInput from "./search-input";
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between h-full w-full">
+    <nav className="flex items-center justify-between h-full w-full gap-y-4">
       <div className="flex gap-3 items-center shrink-0 pr-6">
         <Link href="/">
           <Image src="/livedocs.svg" alt="Logo" width={190} height={110} />
         </Link>
-        {/* <h3 className="text-xl">Docs</h3> */}
       </div>
       <SearchInput />
       <div />
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,33 @@
+"use client";
+
 import React from "react";
-import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./templates-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 function Home() {
+  const documents = useQuery(api.documents.get);
+
+  if(documents === undefined){
+    return(
+      <div className="flex items-center justify-center min-h-screen bg-white">
+        <img src="/livedocs.svg" alt="logo" className="w-[800px] animate-pulse" />
+      </div>
+    );
+  }
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="fixed top-0 left-0 right-0 z-10 h-16bg-white p-4 ">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        <div className="p-4 space-y-2">
+          {documents?.map((document) => (
+            <span key={document._id}>{document.title}</span>
+          ))}
+        </div>
       </div>
     </div>
   );
