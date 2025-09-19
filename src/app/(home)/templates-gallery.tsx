8da572ca34380aa1b31ec033389c625de869d364
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-09-19 13:16:45 +0530

    main

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -1,9 +1,64 @@
 "use client";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
+
+const templates = [
+  { id: "blank", label: "Blank Document", imageUrl: "/blank-document.svg" },
+  { id: "software-proposal", label: "Software Development Proposal", imageUrl: "/software-proposal.svg" },
+  { id: "project-proposal", label: "Project Proposal", imageUrl: "/project-proposal.svg" },
+  { id: "business-letter", label: "Business Letter", imageUrl: "/business-letter.svg" },
+  { id: "resume", label: "Resume", imageUrl: "/resume.svg" },
+  { id: "cover-letter", label: "Cover Letter", imageUrl: "/cover-letter.svg" },
+  { id: "letter", label: "Letter", imageUrl: "/letter.svg" },
+
+];
+
 export const TemplateGallery = () => {
-    return(
-        <div className="bg-[#F1F3F4]">
-            <div className="">Hello</div>
-        </div>
-    );
-}
+  const isCreated = false;
+  return (
+    <div className="bg-[#F1F3F4]">
+      <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
+        <h3 className="font-medium">Choose Template</h3>
+        <Carousel>
+          <CarouselContent className="-ml-4">
+            {templates.map((template) => (
+              <CarouselItem
+                key={template.id}
+                className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.285714%] pl-4"
+              >
+                <div
+                  className={cn(
+                    "aspect-[3/4] flex flex-col gap-y-2.5",
+                    isCreated && "pointer-events-none opacity-50"
+                  )}
+                >
+                  <button
+                    disabled={isCreated}
+                    onClick={() => {}}
+                    style={{
+                      backgroundImage: `url(${template.imageUrl})`,
+                      backgroundPosition: "center",
+                      backgroundSize: "cover",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                    className="size-full hover:border-blue-500 rounded-sm border hover:bg-blue-50 transition flex flex-col items-center justify-center gap-y-4 bg-white"
+                  ></button>
+                  <p className="text-sm font-medium truncate">
+                    {template.label}
+                  </p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
