366d53ff641dd8ab505769d0939f24824b2cfdd9
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-09-20 11:54:47 +0530

    main

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,10 +1,27 @@
 import { mutation, query } from "./_generated/server";
 
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 export const create = mutation({
-args: { title: v.optional(v.string()) },
-handler: async () => {},
-}) ;
+  args: {
+    title: v.optional(v.string()),
+    initialContent: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    return await ctx.db.insert("documents", {
+      title: args.title ?? "Untitled Document",
+      ownerId: user.subject,
+      initialContent: args.initialContent,
+
+    })
+
+  },
+});
 
 export const get = query({
   handler: async (ctx) => {
