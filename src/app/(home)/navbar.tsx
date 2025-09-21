ad8b2cd43ccbddffcbe00cb11f3e76b45bc172de
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-09-21 13:20:33 +0530

    main

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import SearchInput from "./search-input";
-import { UserButton } from "@clerk/nextjs"
+import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
 export const Navbar = () => {
   return (
     <nav className="flex items-center justify-between h-full w-full gap-y-4">
@@ -11,7 +11,15 @@ export const Navbar = () => {
         </Link>
       </div>
       <SearchInput />
-      <UserButton />
+      <div className="flex gap-3 items-center pl-6">
+        <OrganizationSwitcher 
+        afterSelectOrganizationUrl="/"
+        afterLeaveOrganizationUrl="/"
+        afterSelectPersonalUrl="/"
+        afterCreateOrganizationUrl="/"
+        />
+        <UserButton />
+      </div>
     </nav>
   );
 };
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,9 +6,11 @@ import { TemplateGallery } from "./templates-gallery";
 import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { DocumentsTable } from "./document-table";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 function Home() {
-  const {results, status, loadMore} = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 });
+  const [search] = useSearchParam();
+  const {results, status, loadMore} = usePaginatedQuery(api.documents.get, { search }, { initialNumItems: 5 });
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
